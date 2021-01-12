0c8dab82ed5c5631d25c67a863eb9581046d0f7e
Marcel <MTRNord@users.noreply.github.com>
2021-01-12 19:38:58 +0100

    Update errors.rs

--- mrsbfh/src/errors.rs
+++ mrsbfh/src/errors.rs
@@ -7,3 +7,11 @@ pub enum ConfigError {
     #[error(transparent)]
     SerdeError(#[from] serde_yaml::Error),
 }
+
+#[derive(Error, Debug)]
+pub enum SessionError {
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    #[error(transparent)]
+    SerdeError(#[from] serde_json::Error),
+}
