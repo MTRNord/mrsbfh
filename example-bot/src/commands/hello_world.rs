bdcfcd3a8c25931db267094624136ed2c9503f9b
MTRNord <mtrnord1@gmail.com>
2022-03-12 19:58:18 +0100

    Remove unused where

--- example-bot/src/commands/hello_world.rs
+++ example-bot/src/commands/hello_world.rs
@@ -1,4 +1,3 @@
-use crate::config::Config;
 use crate::errors::Error;
 use matrix_sdk::ruma::events::{room::message::MessageEventContent, AnyMessageEventContent};
 use mrsbfh::commands::command;
@@ -6,10 +5,7 @@ use mrsbfh::commands::extract::Extension;
 use std::sync::Arc;
 
 #[command(help = "`!hello_world` - Prints \"hello world\".")]
-pub async fn hello_world<'a>(Extension(tx): Extension<Arc<mrsbfh::Sender>>) -> Result<(), Error>
-where
-    Config<'a>: mrsbfh::config::Loader + Clone,
-{
+pub async fn hello_world<'a>(Extension(tx): Extension<Arc<mrsbfh::Sender>>) -> Result<(), Error> {
     let content =
         AnyMessageEventContent::RoomMessage(MessageEventContent::notice_plain("Hello World!"));
 
